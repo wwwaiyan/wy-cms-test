c6c8829a8803855312b94db8d2a3dee974615f51
wwwaiyan <hello.waiyansoe@gmail.com>
2024-09-06 05:10:42 +0000

    hestia theme added

---
+++ vendor/codeinwp/themeisle-sdk/assets/js/build/promos/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-plugins'), 'version' => '2cd9ed3afc77de83f989');
